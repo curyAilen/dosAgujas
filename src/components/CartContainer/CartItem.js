883f8ca54925fd8fb264fc7b2637889e657b1347
curyAilen <cury.ailena@gmail.com>
2023-05-16 18:05:24 -0300

    Repusheado

---
+++ src/components/CartContainer/CartItem.js
@@ -0,0 +1,58 @@
+import { Button, Card, Image, Row, Col, Container } from "react-bootstrap";
+import FormOrder from "./FormOrder";
+import { useContext } from "react";
+import { contexto } from "../CustomProvider";
+
+function CartItem() {
+ 
+  return (
+    <Container>
+      <Row>
+        <Col xl={6}>
+          <h3>Mi carrito</h3>
+          <hr style={{ width: "50%" }}></hr>
+          {carrito.map((item) => (
+            <div
+              style={{ width: "18rem", margin: "5px " }}
+              className="d-flex"
+              key={item.id}
+            >
+              <div className="card-header">
+                <Card.Img src={item.img} />
+                <Card.Text>{item.categoria}</Card.Text>
+              </div>
+              <Card.Body className="m-5">
+                <Card.Title>{item.nombre}</Card.Title>
+                <Card.Text>Precio: ${item.precio}</Card.Text>
+              </Card.Body>
+              <Card.Footer className="d-flex justify-content-end align-items-center">
+                <Button
+                  variant="secondary"
+                  onClick={() => handleDecrement(item.id)}
+                >
+                  -
+                </Button>
+                <h4 className="mx-2">{item.cantidad}</h4>
+                <Button
+                  variant="secondary"
+                  onClick={() => handleIncrement(item.id)}
+                >
+                  +
+                </Button>
+
+                <Button variant="danger" onClick={() => handleRemove(item.id)}>
+                  Remover
+                </Button>
+              </Card.Footer>
+            </div>
+          ))}
+        </Col>
+        <Col xl={6}>
+          <FormOrder />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default CartItem;
