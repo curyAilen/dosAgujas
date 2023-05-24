de126f0de00eb35dd5586695e3e714ee3729e231
curyAilen <cury.ailena@gmail.com>
2023-05-24 12:01:29 -0300

    Proyecto final Ailen Cury - Coder House React

--- src/components/CartContainer/FormOrder.js
+++ src/components/CartContainer/FormOrder.js
@@ -9,6 +9,10 @@ const FormOrder = () => {
   const [direccion, setDireccion] = useState('');
   const [localidad, setLocalidad] = useState('');
   const [codigoPostal, setCodigoPostal] = useState('');
+  const [cardName, setCardName] = useState('');
+  const [cardNumber, setCardNumber] = useState('');
+  const [cardExpiry, setCardExpiry] = useState('');
+  const [cardCvc, setCardCvc] = useState('');
   const [showModal, setShowModal] = useState(false);
   const [formErrors, setFormErrors] = useState({});
   const { cart, clearCart } = useContext(CartContext);
@@ -34,6 +38,27 @@ const FormOrder = () => {
     return '';
   };
 
+  const validateCardNumber = (value) => {
+    if (!value || !/^\d{16}$/.test(value)) {
+      return 'Ingrese un número de tarjeta válido, recuerde que son 16 digitos.';
+    }
+    return '';
+  };
+
+  const validateCardExpiry = (value) => {
+    if (!value || !/^\d{2}\/\d{2}$/.test(value)) {
+      return 'Ingrese una fecha de vencimiento válida (MM/AA).';
+    }
+    return '';
+  };
+
+  const validateCardCvc = (value) => {
+    if (!value || !/^\d{3}$/.test(value)) {
+      return 'Ingrese un código de seguridad válido.';
+    }
+    return '';
+  };
+
   const agregarDocumento = async (fecha, items, total, user) => {
     try {
       const nuevoDocumento = await addDoc(collection(db, "order"), {
@@ -42,6 +67,7 @@ const FormOrder = () => {
         total: total,
         user: user,
       });
+      return nuevoDocumento;
     } catch (error) {
       console.error(error);
     }
@@ -52,19 +78,36 @@ const FormOrder = () => {
   
     const emailError = validateEmail(email);
     const direccionError = validateLength(direccion, 6);
-    const localidadError = validateLength(localidad, 6);
+    const localidadError = validateLength(localidad, 4);
     const codigoPostalError = validateNumber(codigoPostal);
+    const cardNameError = validateLength(cardName, 2);
+    const cardNumberError = validateCardNumber(cardNumber);
+    const cardExpiryError = validateCardExpiry(cardExpiry);
+    const cardCvcError = validateCardCvc(cardCvc);
   
     const errors = {
       email: emailError,
       direccion: direccionError,
       localidad: localidadError,
       codigoPostal: codigoPostalError,
+      cardName: cardNameError,
+      cardNumber: cardNumberError,
+      cardExpiry: cardExpiryError,
+      cardCvc: cardCvcError,
     };
   
     setFormErrors(errors);
   
-    if (emailError || direccionError || localidadError || codigoPostalError) {
+    if (
+      emailError ||
+      direccionError ||
+      localidadError ||
+      codigoPostalError ||
+      cardNameError ||
+      cardNumberError ||
+      cardExpiryError ||
+      cardCvcError
+    ) {
       return;
     }
   
@@ -108,6 +151,10 @@ const FormOrder = () => {
     setDireccion("");
     setLocalidad("");
     setCodigoPostal("");
+    setCardName("");
+    setCardNumber("");
+    setCardExpiry("");
+    setCardCvc("");
     setFormErrors({});
   };
 
@@ -195,15 +242,89 @@ const FormOrder = () => {
           </Col>
         </Form.Group>
 
-        <Button type="submit">Realizar Pedido</Button>
+        <Form.Group as={Row} className="mb-3" controlId="cardName">
+          <Form.Label column sm={2}>
+            Nombre en la tarjeta
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control
+              type="text"
+              placeholder="Ingrese tipo de tarjeta (Visa, Mastercard, etc)"
+              value={cardName}
+              onChange={(e) => setCardName(e.target.value)}
+              isInvalid={!!formErrors.cardName}
+            />
+            <Form.Control.Feedback type="invalid">
+              {formErrors.cardName}
+            </Form.Control.Feedback>
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3" controlId="cardNumber">
+          <Form.Label column sm={2}>
+            Número de tarjeta
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control
+              type="text"
+              placeholder="Ingrese el número de tarjeta"
+              value={cardNumber}
+              onChange={(e) => setCardNumber(e.target.value)}
+              isInvalid={!!formErrors.cardNumber}
+            />
+            <Form.Control.Feedback type="invalid">
+              {formErrors.cardNumber}
+            </Form.Control.Feedback>
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3" controlId="cardExpiry">
+          <Form.Label column sm={2}>
+            Fecha de vencimiento
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control
+              type="text"
+              placeholder="Ingrese la fecha de vencimiento de la tarjeta (MM/AA)"
+              value={cardExpiry}
+              onChange={(e) => setCardExpiry(e.target.value)}
+              isInvalid={!!formErrors.cardExpiry}
+            />
+            <Form.Control.Feedback type="invalid">
+              {formErrors.cardExpiry}
+            </Form.Control.Feedback>
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3" controlId="cardCvc">
+          <Form.Label column sm={2}>
+            Código de seguridad
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control
+              type="text"
+              placeholder="Ingrese el código de seguridad de la tarjeta"
+              value={cardCvc}
+              onChange={(e) => setCardCvc(e.target.value)}
+              isInvalid={!!formErrors.cardCvc}
+            />
+            <Form.Control.Feedback type="invalid">
+              {formErrors.cardCvc}
+            </Form.Control.Feedback>
+          </Col>
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Realizar Pedido
+        </Button>
       </Form>
 
       <Modal show={showModal} onHide={handleCloseModal}>
         <Modal.Header closeButton>
-          <Modal.Title>Pedido Realizado</Modal.Title>
+          <Modal.Title>¡Pedido realizado!</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          Su pedido ha sido realizado exitosamente. ¡Gracias por su compra!
+          <p>Su compra fue gestionada y aprovada con exito.</p>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleCloseModal}>
